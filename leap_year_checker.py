46325049d44e35462b00846e5db6d61a79048fc4
roxygen123 <36231597+roxygen123@users.noreply.github.com>
2020-03-07 14:31:54 +0000

    Create leap_year_checker.py

---
+++ leap_year_checker.py
@@ -0,0 +1,9 @@
+year = input("year you need to check:")
+year_number = int(year)
+
+def leap_year_checker():
+    if year_number % 4 == 0:
+        print(year_number,"it's a leap year")
+    else: 
+        print(year_number,"it's not a leap year")
+leap_year_checker()
